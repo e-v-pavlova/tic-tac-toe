324fbcfb756ddebe60c4f9ae83f42927c1c4a599
e-v-pavlova <e-v-pavlova@cat>
2019-05-28 11:44:22 +0300

    add class Game
    added Game.js

---
+++ js/Game.js
@@ -0,0 +1,9 @@
+class Game {
+    constructor (){
+        this.player1 = new Player(1,'img/x.jpeg');
+        this.player2 = new Player(2,'img/o.jpeg');
+        this.manager = new Manager(this.player1, this.player2);
+        this.playField = new PlayField();
+        this.playField.init();
+    }
+}
--- js/tictactoe.js
+++ js/tictactoe.js
@@ -1,7 +1,1 @@
-const playField = new PlayField();
-playField.render();
-
-const player1 = new Player(1,'img/x.jpeg');
-const player2 = new Player(2,'img/o.jpeg');
-
-const manager = new Manager(player1, player2);
+new Game;
